d244a0b6e44adcd4d4dc3a1b1de5a0c07ae8974d
aitoAarni <aarni.rechardt@gmail.com>
2025-05-13 12:42:31 +0200

    letters can be individually colored now

--- frontend/src/components/TypingBox.tsx
+++ frontend/src/components/TypingBox.tsx
@@ -1,29 +1,51 @@
 import { useEffect, useState } from "react"
+import styles from "./TypingBox.module.scss"
 
 interface TypingBoxProps {
     text?: string
 }
-const TypingBox = ({text} : TypingBoxProps) => {
-    const [letterList, setLetterList] = useState<string[]>([])
+const TypingBox = ({ text }: TypingBoxProps) => {
+    const [charList, setCharList] = useState<string[]>([])
+    const [currentIndex, setCurrentIndex] = useState(0)
+    const [typedCorrectly, setTypedCorrectly] = useState<boolean[]>([])
+
     useEffect(() => {
         if (text?.length) {
-            setLetterList(text?.split(''))
+            setCharList(text?.split(""))
+            setTypedCorrectly(new Array(text.length).fill(null))
         }
     }, [text])
+    useEffect(() => {
+        const copy = [...typedCorrectly]
+        copy[currentIndex] = true
+        setTypedCorrectly(copy)
+    }, [currentIndex])
     return (
         <div>
             {text} <br />
-            {letterList.map((letter, index) => {
+            {charList.map((char, index) => {
+                const caretClass =
+                    currentIndex == index ? styles.caretActive : styles.caret
+                let charClass
+                if (typedCorrectly[index] == true) {
+                    charClass = styles.charCorrect
+                } else if (typedCorrectly[index] == false) {
+                    charClass = styles.charIncorrect
+                } else {
+                    charClass = styles.charUntyped
+                }
+
                 return (
-                    <span key={index} >
-                        {letter}
+                    <span key={index}>
+                        <span className={caretClass}></span>
+                        <span className={charClass}>
+                            {char}
+                        </span>
                     </span>
                 )
             })}
         </div>
     )
 }
-
-
 
 export default TypingBox
