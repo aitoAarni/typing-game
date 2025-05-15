9b2841750ffd7d15c0d068abdd9a2391f9723353
aitoAarni <aarni.rechardt@gmail.com>
2025-05-15 17:35:26 +0200

    removed redundant code

--- frontend/__mocks__/styleMock.js
+++ frontend/__mocks__/styleMock.js
@@ -2,8 +2,4 @@
 module.exports = {
     // This is a mock for CSS modules
     // You can customize it based on your needs
-    className: 'mock-class',
-    localIdentName: '[name]__[local]__[hash:base64:5]',
-    content: '',
-    charUntyped: "charUntyped"
 };
