9e75d5067615d40e18dd6b9953b58d7f9abad0a0
aitoAarni <aarni.rechardt@gmail.com>
2025-05-15 21:30:37 +0200

    creating a view to manage typing related comopnents

---
+++ frontend/src/components/TypingView.tsx
@@ -0,0 +1,7 @@
+import { useState } from "react"
+import styles from "./TypingView.module.css"
+
+const TypingView = () => {
+    const [isTyping, setIsTyping] = useState<boolean>(ture)
+    return <div className={styles.container}></div>
+}
