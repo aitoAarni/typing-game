a40b4ef75ea6b5aca347ef87fced7fb981d4f974
aitoAarni <aarni.rechardt@gmail.com>
2025-05-22 17:51:57 +0200

    fixed docker setup

--- frontend/src/components/GoogleLogin.tsx
+++ frontend/src/components/GoogleLogin.tsx
@@ -1,9 +1,13 @@
 import { CredentialResponse, GoogleLogin } from "@react-oauth/google"
-
+import { jwtDecode } from "jwt-decode"
 const GoogleLoginComponent = () => {
     const onSuccess = (response: CredentialResponse) => {
         console.log("Login Success: currentUser:", response)
         console.log("credentialResponse", response.credential)
+        if (response.credential) {
+            const decoded = jwtDecode(response.credential)
+            console.log("Decoded JWT:", decoded)
+        }
     }
 
     const onError = () => {
--- frontend/src/types/types.ts
+++ frontend/src/types/types.ts
@@ -2,3 +2,10 @@ import { z } from "zod";
 import { WordDefinitionSchema } from "./TypeGuards";
 
 export type WordDefinition = z.infer<typeof WordDefinitionSchema>
+
+export type User = {
+    id: string
+    username: string
+    emai: string
+
+}
