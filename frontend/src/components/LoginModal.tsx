b1da0c1d5e6258d4411eb07a8e6060748c412d92
aitoAarni <aarni.rechardt@gmail.com>
2025-05-24 22:16:58 +0200

    add login modal

---
+++ frontend/src/components/LoginModal.tsx
@@ -0,0 +1,46 @@
+import styles from "./LoginModal.module.scss"
+import loginGoogle from "../services/Login"
+import useAuthUpdate from "../hooks/useAuthUpdate"
+import { CredentialResponse } from "@react-oauth/google"
+import LocalStorage from "../services/LocalStorageService"
+import { GoogleLogin } from "@react-oauth/google"
+
+interface LoginModalProps {
+    closeModal: () => void
+}
+
+const LoginModal = ({ closeModal }: LoginModalProps) => {
+    console.log("LoginModal rendered")
+    const authUpdate = useAuthUpdate()
+    const handleLogin = async (credentials: CredentialResponse) => {
+        try {
+            const { token, user } = await loginGoogle(credentials)
+            LocalStorage.setToken(token)
+            LocalStorage.setUser(user)
+            authUpdate()
+        } catch (error) {
+            console.error("Login failed", error)
+        }
+    }
+
+    const onError = () => {
+        console.log("Login Failed")
+    }
+    return (
+        <div className={styles.container} onClick={closeModal}>
+            <div className={styles.modal} onClick={e => e.stopPropagation()}>
+                <button className={styles.closeButton} onClick={closeModal}>
+                    close
+                </button>
+                <p className={styles.text}>Login with google</p>
+                <GoogleLogin
+                    containerProps={{ className: styles.google }}
+                    onSuccess={handleLogin}
+                    onError={onError}
+                />
+            </div>
+        </div>
+    )
+}
+
+export default LoginModal
