adf57d392aa5e4a2e2fd3cd6fbe917546ce9dca3
aitoAarni <aarni.rechardt@gmail.com>
2025-06-12 17:40:36 +0300

    add prod envionment

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,7 +1,11 @@
-export const API_URL =
-    import.meta.env.MODE === "development" ? "http://localhost:8080/api" : ""
+export const API_URL: string =
+    import.meta.env.MODE === "development"
+        ? "http://localhost:8080/api"
+        : import.meta.env.VITE_API_URL
 
-export const OAuthID =
+console.log("API_URL:", API_URL)
+
+export const OAuthID: string =
     import.meta.env.MODE === "development"
         ? "732089055389-thtclgrih0i7o6tsen7aqc2rsfqu4ufv.apps.googleusercontent.com"
-        : ""
+        : "732089055389-thtclgrih0i7o6tsen7aqc2rsfqu4ufv.apps.googleusercontent.com"
