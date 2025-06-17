04bc6964f88e1e23cb091e7d2235cbb81fb66153
aitoAarni <aarni.rechardt@gmail.com>
2025-06-17 16:23:46 +0300

    adding new definitions to db works

--- database-migrations/addDefinitions.js
+++ database-migrations/addDefinitions.js
@@ -1,1 +1,41 @@
+import { Client } from "pg"
+import "dotenv/config"
+import fs from "fs/promises"
 
+const definitions = JSON.parse(
+    await fs.readFile("./data/definitions.json", "utf-8")
+)
+const databaseUrl = process.env.DATABASE_URL
+
+const client = new Client({
+    connectionString: databaseUrl,
+})
+
+const postDefinitions = async definitions => {
+    try {
+        await client.connect()
+
+        const query = `INSERT INTO definitions (word, definition, sentence) VALUES
+        ${definitions
+            .map((_, i) => `($${i * 3 + 1}, $${i * 3 + 2}, $${i * 3 + 3})`)
+            .join(", ")}
+        ON CONFLICT (word) DO UPDATE 
+        SET definition = EXCLUDED.definition, sentence = EXCLUDED.sentence;`
+
+        const values = definitions.flatMap(({ word, definition, sentence }) => [
+            word,
+            definition,
+            sentence,
+        ])
+        console.log("sending word defiinitions to the database...")
+        await client.query(query, values)
+        console.log("Definitions successfully added to the database.")
+    } catch (error) {
+        console.error("Error connecting to the database:", error)
+    } finally {
+        await client.end()
+        console.log("Database connection closed.")
+    }
+}
+
+postDefinitions(definitions)
